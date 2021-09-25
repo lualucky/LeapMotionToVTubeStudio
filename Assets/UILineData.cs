d079e4d8f497a7e48e57c1c51372713d69520d46
DESKTOP-2HR41AL\Lua <luavtuber@gmail.com>
2021-09-25 01:10:20 -0700

    Add new files including the Leap Motion unity modules in full except the skeleton hands

---
+++ Assets/UILineData.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILineData : MonoBehaviour
+{
+    public Toggle EnableToggle;
+    public Text NameText;
+    public Text ParameterNameText;
+    public InputField OffsetInput;
+    public Text ValueText;
+
+    public List<GameObject> ToggleObjects;
+
+    private void Start()
+    {
+        EnableToggle.onValueChanged.AddListener(Toggle);
+    }
+
+    public void Toggle(bool enable)
+    {
+        foreach (GameObject obj in ToggleObjects)
+        {
+            obj.SetActive(enable);
+        }
+    }
+
+    public void SetName(string name)
+    {
+        NameText.text = name;
+    }
+
+    public string Name()
+    {
+        return NameText.text;
+    }
+
+    public void SetParameterName(string parameterName)
+    {
+        ParameterNameText.text = parameterName;
+    }
+
+    public string ParameterName()
+    {
+        return ParameterNameText.text;
+    }
+
+    public float Offset()
+    {
+        if(OffsetInput)
+            return float.Parse(OffsetInput.text);
+        return 0;
+    }
+
+    public void RegisterOffsetCallback(UnityEngine.Events.UnityAction<string> offsetEvent)
+    {
+        if(OffsetInput)
+            OffsetInput.onValueChanged.AddListener(offsetEvent);
+    }
+
+    public void SetValue(float value)
+    {
+        ValueText.text = "" + value;
+    }
+
+    public bool Enabled()
+    {
+        return EnableToggle.isOn;
+    }
+}
